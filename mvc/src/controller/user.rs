7401861142af7f54ba7396faa954773715193445
109382921@qq.com <109382921@qq.com>
2024-03-20 14:27:14 +0800

    login

--- mvc/src/controller/user.rs
+++ mvc/src/controller/user.rs
@@ -1,13 +1,14 @@
 use crate::models::user::User;
 
+use rocket::form::Form;
 use rocket::post;
 use rocket::request::{self, FromRequest, Outcome, Request};
 use rocket::serde::json::serde_json::json;
 use rocket::serde::json::Value;
 use rocket::Error;
 
-#[post("/login")]
-pub async fn login() -> Value {
+#[post("/login", data = "<user>")]
+pub fn login(user: Form<User>) -> Value {
     json!({"message":"login"})
 }
 
